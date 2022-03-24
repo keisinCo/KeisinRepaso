d5b3b6178112094295b7fc6e31e3b178b7565ab1
keisinCo <101893995+keisinCo@users.noreply.github.com>
2022-03-24 17:03:54 -0500

    Clase 24/03/2022

---
+++ 2022032024 Variables.php
@@ -0,0 +1,12 @@
+<?php
+//Definimos una variable con texto
+$miVariable = "Hola variable";
+//Visualizamos la variable del explorador
+echo $miVariable;
+//Otro ejemplo de variable
+echo"<br>";
+$miValor = 2;
+//visualizamos sin embargho realizamos una multiplicasion por * 3
+echo 3*$miValor;a
+
+?>
---
+++ 20220324 PRACTICA.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+<body>
+    <h1>Practica en php</h1>
+    <?php
+    //INICIA CODIGO EN PHP
+    //Esto es un comentario
+     /*Todo lo que este aqui dentro
+     se considera un comentario
+     */
+    //COMANDO ECHO DEVUELVE UN VALOR
+    echo"Hola soy un parrafo";
+    echo "<br>";  //salto de line    
+                  /*que no se puede en html*/
+
+    //TERMINA PHP
+
+    ?>
+    </body>
+
+
+</head>
